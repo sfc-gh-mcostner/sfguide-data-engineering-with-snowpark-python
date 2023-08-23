b6303d04a21492917137cbed71e5d3bed2f89696
sfc-gh-mcostner <124079838+sfc-gh-mcostner@users.noreply.github.com>
2023-08-23 10:39:51 -0400

    hol update

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -9,25 +9,11 @@
 # SNOWFLAKE ADVANTAGE: Python UDFs (with third-party packages)
 # SNOWFLAKE ADVANTAGE: SnowCLI (PuPr)
 
-import sys
-
-def main(temp_f: float) -> float:
-    return (float(temp_f) - 32) * (5/9)
-
-
-# For local debugging
-# Be aware you may need to type-convert arguments if you add input parameters
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        print(main(*sys.argv[1:]))  # type: ignore
-    else:
-        print(main())  # type: ignore
-
 # import sys
-# from scipy.constants import convert_temperature
 
 # def main(temp_f: float) -> float:
-#     return convert_temperature(float(temp_f), 'F', 'C')
+#     return (float(temp_f) - 32) * (5/9)
+
 
 # # For local debugging
 # # Be aware you may need to type-convert arguments if you add input parameters
@@ -36,3 +22,17 @@ if __name__ == '__main__':
 #         print(main(*sys.argv[1:]))  # type: ignore
 #     else:
 #         print(main())  # type: ignore
+
+import sys
+from scipy.constants import convert_temperature
+
+def main(temp_f: float) -> float:
+    return convert_temperature(float(temp_f), 'F', 'C')
+
+# For local debugging
+# Be aware you may need to type-convert arguments if you add input parameters
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print(main(*sys.argv[1:]))  # type: ignore
+    else:
+        print(main())  # type: ignore
